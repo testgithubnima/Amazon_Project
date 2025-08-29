c8a73233996d113f888ea32638f8c2a41e346265
mani <95521635+testgithubnima@users.noreply.github.com>
2025-08-29 12:22:08 +0530

    Update AmazonLogin_WithoutPOM.java

--- src/main/java/com/luv/amazon/AmazonLogin_WithoutPOM.java
+++ src/main/java/com/luv/amazon/AmazonLogin_WithoutPOM.java
@@ -1,10 +1,4 @@
 package com.luv.amazon;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
 public class AmazonLogin_WithoutPOM {
 	
 
