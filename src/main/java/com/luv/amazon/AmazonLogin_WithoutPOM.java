6677342a9e37a8075f7ae2d6bc785998f46f9b15
mani <95521635+testgithubnima@users.noreply.github.com>
2025-08-29 12:21:46 +0530

    Update AmazonLogin_WithoutPOM.java

--- src/main/java/com/luv/amazon/AmazonLogin_WithoutPOM.java
+++ src/main/java/com/luv/amazon/AmazonLogin_WithoutPOM.java
@@ -6,15 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class AmazonLogin_WithoutPOM {
-
-	public void loginAmazon() {
-		ChromeDriver driver=new ChromeDriver();
-		driver.get("https://www.amazon.in/");
-		driver.manage().window().maximize();
-		WebElement login = driver.findElement(By.id("nav-link-accountList-nav-line-1"));
-		login.click();
-	}
-	
 	
 
 }
